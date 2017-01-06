048a63a64ca12316074bd4f830fbfec27aaedc52
Caleb Flynn <flynn37@purdue.edu>
2017-01-05 19:49:02 -0500

    edits to header and the styling for it

--- wp-content/themes/purdue-cru/header.php
+++ wp-content/themes/purdue-cru/header.php
@@ -23,27 +23,18 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'purdue-cru' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+  <header id="masthead" class="site-header" role="banner">
+    <div class="header-wrapper">
+      <div class="site-branding">
+        <a href="http://www.purduecru.com/">
+          <img src="wp-content/themes/purdue-cru/images/logo.jpg" id="logo">
+        </a>
+      </div><!-- .site-branding -->
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'purdue-cru' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+      <nav id="site-navigation" class="main-navigation" role="navigation">
+        <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+      </nav><!-- #site-navigation -->
+    </div><!--header-wrapper-->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">

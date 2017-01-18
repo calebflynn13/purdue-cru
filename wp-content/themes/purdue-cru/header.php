8f46e6eb5cc4d85e605bf7e59f80ed6a36648aaf
Caleb Flynn <flynn37@purdue.edu>
2017-01-18 15:08:52 -0500

    update tab formatting

--- wp-content/themes/purdue-cru/header.php
+++ wp-content/themes/purdue-cru/header.php
@@ -21,28 +21,29 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'purdue-cru' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'purdue-cru' ); ?></a>
 
-  <?php if(is_front_page()): ?>
-    <header id="masthead" class="front site-header" role="banner">
-  <?php else: ?>
-    <header id="masthead" class="site-header" role="banner">
-  <?php endif; ?>
+    <?php if(is_front_page()): ?>
+        <header id="masthead" class="front site-header" role="banner">
+    <?php else: ?>
+        <header id="masthead" class="site-header" role="banner">
+    <?php endif; ?>
     <div class="header-wrapper">
-      <div class="site-branding">
-        <a href="http://www.purduecru.com/">
-          <?php
-            if(is_front_page()): ?>
-            <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_white.png" class="logo">
-          <?php
-            else: ?>
-            <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_dark.png" class="logo">
-          <?php endif; ?>
-        </a>
-      </div><!-- .site-branding -->
+        <div class="site-branding">
+            <a href="http://www.purduecru.com/">
+                <?php
+                    if(is_front_page()): ?>
+                        <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_white.png" class="logo">
+                <?php
+                    else: ?>
+                    <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_dark.png" class="logo">
+                <?php endif; ?>
+            </a>
+        </div><!-- .site-branding -->
         <nav id="site-navigation" class="main-navigation" role="navigation">
-        <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-      </nav><!-- #site-navigation -->
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+        </nav><!-- #site-navigation -->
     </div><!--header-wrapper-->
+
   </header><!-- #masthead -->
 

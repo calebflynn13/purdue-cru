13665ab7def153de6e98a5ea61497396eff1db99
Caleb Flynn <flynn37@purdue.edu>
2017-04-09 04:25:24 -0400

    dumb formatting in footer.php file

--- wp-content/themes/purdue-cru/footer.php
+++ wp-content/themes/purdue-cru/footer.php
@@ -11,15 +11,15 @@
 
 ?>
 
-	</div><!-- #content -->
+    </div><!-- #content -->
 
-	<footer id="main-footer" class="site-footer" role="contentinfo">
-    <div class="footer-wrapper">
-      <a href="http://www.purduecru.com/">
-        <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_white.png" class="logo">
-      </a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="main-footer" class="site-footer" role="contentinfo">
+        <div class="footer-wrapper">
+            <a href="http://www.purduecru.com/">
+                <img src="http://www.purduecru.com/wp-content/uploads/2017/01/logo_white.png" class="logo">
+            </a>
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
